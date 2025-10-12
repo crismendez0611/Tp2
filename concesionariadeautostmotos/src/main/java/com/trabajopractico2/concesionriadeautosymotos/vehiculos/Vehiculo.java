3bb2cd04e8ae83f70bc751b8075198d683aaa9c1
Cristian Mendez <cmendez061192@gmail.com>
2025-10-12 19:09:33 -0300

    Add files via upload

--- concesionariadeautostmotos/src/main/java/com/trabajopractico2/concesionriadeautosymotos/vehiculos/Vehiculo.java
+++ concesionariadeautostmotos/src/main/java/com/trabajopractico2/concesionriadeautosymotos/vehiculos/Vehiculo.java
@@ -9,10 +9,15 @@ public abstract class Vehiculo implements Comparable<Vehiculo> {
     private String marca;
     private String modelo;
     private double precio;
-    @Override
+    /**
+     * Metodo para comparar dos vehiculos
+     * Para poder ordenar los vehiculos de forma natural
+     * @param vehiculo2 Vehiculo a comparar
+     * @return int Resultado de la comparacion
+     */
     public int compareTo(Vehiculo vehiculo2) {
-        String vehiculoOriginal=this.marca+" "+this.modelo+" "+this.precio;
-        String vehiculoAComparar=vehiculo2.getMarca()+" "+vehiculo2.getModelo()+" "+vehiculo2.getPrecio();
+        String vehiculoOriginal = this.marca + " " + this.modelo + " " + this.precio;
+        String vehiculoAComparar = vehiculo2.getMarca() + " " + vehiculo2.getModelo() + " " + vehiculo2.getPrecio();
         return vehiculoOriginal.compareTo(vehiculoAComparar);
     }
 }
