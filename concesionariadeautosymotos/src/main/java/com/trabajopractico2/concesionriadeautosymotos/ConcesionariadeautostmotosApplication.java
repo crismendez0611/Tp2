dfac7a88ab9df0fe3128595054e05c9562d9352b
Cristian Mendez <cmendez061192@gmail.com>
2025-10-15 19:15:49 -0300

    Add files via upload

---
+++ concesionariadeautosymotos/src/main/java/com/trabajopractico2/concesionriadeautosymotos/ConcesionariadeautostmotosApplication.java
@@ -0,0 +1,13 @@
+package com.trabajopractico2.concesionriadeautosymotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionariadeautostmotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionariadeautostmotosApplication.class, args);
+	}
+
+}
---
+++ concesionariadeautosymotos/src/test/java/com/trabajopractico2/concecionriadeautosymotos/ConcecionariadeautostmotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajopractico2.concecionriadeautosymotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcecionariadeautostmotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
